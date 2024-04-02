38b93255d9b27c389b8502edf45aceab7d38ddcc
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2024-04-02 09:56:46 +0100

    Completed homepage header and css

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -62,7 +62,6 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR,'templates'),
-                 
                  os.path.join(BASE_DIR,'templates','allauth')
                  ],
         'APP_DIRS': True,
@@ -159,6 +158,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
