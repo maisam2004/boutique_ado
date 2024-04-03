87b1568d36c8c4b2ca042585634c386a07397c03
maisam2004 <46633089+maisam2004@users.noreply.github.com>
2024-04-03 12:39:49 +0100

    Add search functionality and messages

--- products/views.py
+++ products/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import redirect,reverse,render,get_object_or_404
 from .models import Product
+from django.db.models import Q 
+from django.contrib import messages
+
 
 # Create your views here.
 
@@ -8,8 +11,17 @@ def all_products(request):
 
     products = Product.objects.all()
 
+    if request.GET :
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request,"You did not enter any search criterial")
+                return redirect(reverse('products'))
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
     context = {
         'products':products,
+        'search_term':query,
     }
     return render(request , 'products/products.html',context)
 
